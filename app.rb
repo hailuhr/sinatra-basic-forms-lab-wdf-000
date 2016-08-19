0ea040fb3a149f2b02542a9af001db17b9245dd6
Flatiron School <github email address>
2016-08-19 18:12:45 -0400

    done

--- app.rb
+++ app.rb
@@ -1,5 +1,26 @@
 require_relative 'config/environment'
+require_relative 'models/puppy.rb'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  get '/create_puppy.erb' do
+    erb :create_puppy
+  end
+
+  post "/display_puppy" do
+    @name = params[:name]
+    @age = params[:age]
+    @breed = params[:breed]
+
+    erb :display_puppy
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
 
 end
